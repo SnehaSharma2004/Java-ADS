1240e09d8b665a99ff7f575ace87e2749ff16492
SnehaSharma2004 <sneha.sharmax04@gmail.com>
2025-01-30 14:53:26 +0530

    1480

--- RunningSum.java
+++ RunningSum.java
@@ -1,10 +1,13 @@
 package Class;
 
+import java.util.*;
+import java.io.*;
+
 public class RunningSum {
     public static int[] runningSum(int[] nums){
         int[] result=new int[nums.length];
         result[0]=nums[0];
-        for(int i=0;i<nums.length;i++){
+        for(int i=1;i<nums.length;i++){
             result[i]=result[i-1]+nums[i];
         }
         return result;
@@ -12,8 +15,6 @@ public class RunningSum {
     public static void main(String[] args){
         int[] arr={1,2,3,4,5};
         int[] result=runningSum(arr);
-        for(int num:result){
-            System.out.println(num);
-        }
+        System.out.println(Arrays.toString(result));
     }
 }
